725ef117407b275e0c9bd6da6c344f97b432ad44
anamarie18742 <55445811+anamarie18742@users.noreply.github.com>
2020-03-06 12:23:48 +0800

    GUI
    @neilalexvilla @anamarie18742 @taerangelien @johnamagno @neneilgwapo

---
+++ GUI/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>int num = 0;</h2>
+      </header>
+    <header className="inline-div">
+      <textarea cols="40" type="code" placeholder="Input Here:" id="UserInput" className="inline-txtarea">
+      </textarea>
+
+      <textarea cols="40" placeholder="Result:" className="inline-txtarea" >
+      </textarea>
+    </header>
+
+  
+    </div>
+  
+    
+  );
+  }
+
+export default App;
